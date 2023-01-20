87de3ca74d97cc95b313d64c4157ddea774cd53e
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-01-20 17:46:31 -0300

    feat: create navigation in pages

--- curso-react/src/app/pages/dashboard/Dashboard.tsx
+++ curso-react/src/app/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom"
 
 
 
 export const Dashboard = () => {
     return (
+        <div> 
+
         <p>dashboard</p>
+       <button>
+
+        <Link to='/entrar' >Login</Link> 
+       </button>
+        </div> // 8 o link serve para navegar para outa pagina, pode usar tanto p link como o botão
     )
 }
--- curso-react/src/app/pages/login/Login.tsx
+++ curso-react/src/app/pages/login/Login.tsx
@@ -1,7 +1,15 @@
+import {  useNavigate } from "react-router-dom"
+
 export const Login = () => {
+    const navigate = useNavigate();
+
+    const handleclik = () => {
+         navigate('/pagina-inicial')
+    }
     return (
      <div>
         Login
+        <button onClick={handleclik} >Home</button>
      </div>           
     )
 }
