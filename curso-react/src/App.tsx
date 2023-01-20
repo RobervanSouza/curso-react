499e4cfe138bb47e041dad4607889247c32f9107
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-01-20 15:02:13 -0300

    feat: remove file

---
+++ curso-react/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+export const  App = () =>{  // 8 mudando para função quando for importar tem que usar {}
+  return (
+    <div className="App">
+      <header className="App-header">
+   
+        <p>
+       estou aqui
+        </p>
+       
+         
+       
+      </header>
+    </div>
+  );
+}
---
+++ curso-react/src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import {App} from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement // 8  a aplicaçâo roda dento de uma div root
+);
+root.render( // 8 render faz a aplicação carregar
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
