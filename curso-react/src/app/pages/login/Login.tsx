333b18c04a61144323debc6ce4842e104c023aaa
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-01-20 20:08:07 -0300

    feat: starting login screen

--- curso-react/src/app/pages/login/Login.tsx
+++ curso-react/src/app/pages/login/Login.tsx
@@ -1,15 +1,43 @@
-import {  useNavigate } from "react-router-dom"
+
 
 export const Login = () => {
-    const navigate = useNavigate();
-
-    const handleclik = () => {
-         navigate('/pagina-inicial')
+    const handleToOpen = () => {
+        
     }
+   
     return (
-     <div>
-        Login
-        <button onClick={handleclik} >Home</button>
-     </div>           
-    )
+      <div>
+        <form action="">
+          <label htmlFor="">
+            <span>Email</span>
+            <input type="text" placeholder="Digite seu Email" />
+          </label>
+          <label htmlFor="">
+            <span>Senha</span>
+            <input type="text" placeholder="Digite sua Senha" />
+          </label>
+          <button type="button" onClick={handleToOpen}>
+            Entar
+          </button>
+        </form>
+      </div> // esse buton (type="button") aguarda uma função para poder executar a função, a função submit recarrega a pagina não precisa da função
+    );
 }
+
+/*
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+  const navigate = useNavigate();
+
+  const handleclik = () => {
+    navigate("/pagina-inicial");
+  };
+  return (
+    <div>
+      Login
+      <button onClick={handleclik}>Home</button>
+    </div>
+  );
+};
+*/
